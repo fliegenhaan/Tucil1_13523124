d8a39d690e0890fd3408e1c921254a88ebc5fb8a
Muhammad Raihaan Perdana <perdanaraihan96@gmail.com>
2025-02-24 04:15:56 +0700

    feat: constants for color output

---
+++ src/config/Constants.java
@@ -0,0 +1,34 @@
+package config;
+
+public class Constants {
+    // ANSI Color codes untuk output berwarna
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String[] COLORS = {
+        "\u001B[31m", // RED
+        "\u001B[32m", // GREEN
+        "\u001B[33m", // YELLOW
+        "\u001B[34m", // BLUE
+        "\u001B[35m", // PURPLE
+        "\u001B[36m", // CYAN
+        "\u001B[91m", // BRIGHT RED
+        "\u001B[92m", // BRIGHT GREEN
+        "\u001B[93m", // BRIGHT YELLOW
+        "\u001B[94m", // BRIGHT BLUE
+        "\u001B[95m", // BRIGHT PURPLE
+        "\u001B[96m", // BRIGHT CYAN
+        "\u001B[41m", // RED BACKGROUND
+        "\u001B[42m", // GREEN BACKGROUND
+        "\u001B[43m", // YELLOW BACKGROUND 
+        "\u001B[44m", // BLUE BACKGROUND
+        "\u001B[45m", // PURPLE BACKGROUND
+        "\u001B[46m", // CYAN BACKGROUND
+        "\u001B[101m", // BRIGHT RED BACKGROUND
+        "\u001B[102m", // BRIGHT GREEN BACKGROUND
+        "\u001B[103m", // BRIGHT YELLOW BACKGROUND
+        "\u001B[104m", // BRIGHT BLUE BACKGROUND
+        "\u001B[105m", // BRIGHT PURPLE BACKGROUND
+        "\u001B[106m", // BRIGHT CYAN BACKGROUND
+        "\u001B[97m", // WHITE
+        "\u001B[37m"  // GRAY
+    };
+}
